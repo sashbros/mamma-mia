1b011e5c55e799760a6461288b50528a54ca2537
sashbros <sarvadream09.ss@gmail.com>
2022-03-02 14:22:15 -0500

    removed freezer flask

--- server.py
+++ server.py
@@ -3,18 +3,9 @@ from flask import Flask, render_template, jsonify, request, redirect
 import requests
 import json
 from livereload import Server
-from flask_flatpages import FlatPages
-from flask_frozen import Freezer
-
-DEBUG = True
-FLATPAGES_AUTO_RELOAD = DEBUG
-FLATPAGES_EXTENSION = '.md'
 
 app = Flask(__name__)
 
-app.config.from_object(__name__)
-pages = FlatPages(app)
-freezer = Freezer(app)
 
 def fetch_price(coins):
     data = []
@@ -44,5 +35,3 @@ if __name__ == "__main__":
     server = Server(app.wsgi_app)
     server.watch('./static/*')
     server.serve(port=8000)
-    freezer.freeze()
-    # app.run(host='127.0.0.1', port=8000)
